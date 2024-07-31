20089b1a28a36ae2a1e4e1f571a584a3b06984d5
Nicolas Fuentes <nicolasfuentes1195@gmail.com>
2024-07-31 16:51:25 -0300

    Correccion de ejercicio 2

--- Python Inicial/ejercicio2.py
+++ Python Inicial/ejercicio2.py
@@ -8,22 +8,15 @@ import sys
 
 valor1 = sys.argv[1]
 valor1 = int(valor1)
+
 valor2 = sys.argv[2]
 valor2 = int(valor2)
+
 valor3 = sys.argv[3]
 valor3 = int(valor3)
 
-if valor1 % 2 == 0:
-    print("Es un numero par")
-else:
-    print("Es un numero impar")
-
-if valor2 % 2 == 0:
-    print("Es un numero par")
-else:
-    print("Es un numero impar")
-
-if valor3 % 2 == 0:
-    print("Es un numero par")
-else:
-    print("Es un numero impar")
+# Muestra de resultados
+print("El numero si es par mostrara True si es impar mostrara False:")
+print("Numero", valor1, ":", valor1 % 2 == 0)
+print("Numero", valor2, ":", valor2 % 2 == 0)
+print("Numero", valor3, ":", valor3 % 2 == 0)
