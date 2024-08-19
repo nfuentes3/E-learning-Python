7dbd8859438502c0b66eafa2c2fe3e861c394dc3
Nicolas Fuentes <nicolasfuentes1195@gmail.com>
2024-08-19 16:22:38 -0300

    Agregando los ejercicios de la Unidad 3

---
+++ Python Inicial/Unidad 3/ejercicio1.py
@@ -0,0 +1,8 @@
+"""Tome el ejercicio de cálculo de números pares e impares de la unidad 2 y agréguele un bucle al código de forma de simplificarlo. """
+
+#Inicializacion de lista con elementos definidos.
+frutas = ["Naranja","Frutilla","Manzana","Pera"]
+
+print("Lista de frutas en lista: ")
+for fruta in frutas:
+    print(f"* {fruta}")
