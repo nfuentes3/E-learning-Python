cea5c1260962f33f30d6aed112214d031472dcd0
Nicolas Fuentes <nicolasfuentes1195@gmail.com>
2024-08-21 15:33:13 -0300

    Correcion ejercicio 1

--- Python Inicial/Unidad 3/ejercicio1.py
+++ Python Inicial/Unidad 3/ejercicio1.py
@@ -1,9 +1,5 @@
 """Tome el ejercicio de cálculo de números pares e impares de la unidad 2 y agréguele un bucle al código de forma de simplificarlo. """
-
 import sys
-
-import sys
-
 
 #Se crea condicional, con ejecuion por consola: py ejercicio2.py 2 3 8
 for numero in sys.argv[1:]:
