1533d9d3f8f7fa2c11d40f31f5269ea6c351d25a
Nicolas Fuentes <nicolasfuentes1195@gmail.com>
2024-08-26 16:29:58 -0300

    Agregando ejercicios de la unidad 4

--- Python Inicial/Unidad 4/ejercicio1.py
+++ Python Inicial/Unidad 4/ejercicio1.py
@@ -1,2 +1,5 @@
-a = 3
+"""Cree una función lamba que compruebe si un número es par o impar."""
 
+par_impar = lambda num1 : num1 % 2 == 0
+numero = int(input("Indique un numero: "))
+print(f"El numero es par?: {par_impar(numero)}")
